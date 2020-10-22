0ab95a5c05e9e900c4a192df6b9e68026b296de9
Stach Redeker <64889011+StachRedeker@users.noreply.github.com>
2020-10-22 18:10:42 +0200

    Delete background.js

--- V1/nl_NL/background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    chrome.browserAction.setBadgeText({text: `${request.counter}`});
-});
